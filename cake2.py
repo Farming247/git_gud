874c0799583c91c36bce2e1755dd6be3f947aec5
jae <jaekim2013@gmail.com>
2019-02-12 14:49:38 -0500

    Another checkpoint

--- cake2.py
+++ cake2.py
@@ -3,8 +3,13 @@
 # Date: 2/12/2019
 
 def main():
+   userCake = input("Did you bake this cake? (Bake/Buy): ")
+   print(userCake)
 
-    userCake = input("Did you bake this cake? (Bake/Buy): ")
-    print(userCake)
-
+   if userCake == "Buy":
+      userStore = input("Where did you but this cake?")
+      print(userStore)
+      print("Read the label from " + userStore + " to find out the sweetness!")
+      
+    
 main()
