7ef9b1d121b1a285f2815bb04b212153c94c5f87
jae <jaekim2013@gmail.com>
2019-02-12 15:46:29 -0500

    Final product of lab 3

--- cake2.py
+++ cake2.py
@@ -3,13 +3,28 @@
 # Date: 2/12/2019
 
 def main():
-   userCake = input("Did you bake this cake? (Bake/Buy): ")
+   userCake = input("Did you bake this cake? (bake/buy): ")
    print(userCake)
 
-   if userCake == "Buy":
+   if userCake == "buy":
       userStore = input("Where did you but this cake?")
       print(userStore)
       print("Read the label from " + userStore + " to find out the sweetness!")
-      
+      print("Regardless of how sweet you like it, enjoy your cake!")
+
+   elif userCake == "bake":
+      userSugar = float(input("How many cups of sugar did you use?"))
+      print(userSugar)
+
+      if userSugar <=2.49:
+         print("You must not like sweet things!")
+         print("Regardless of how sweet you like it, enjoy your cake!")
+
+      elif userSugar >= 2.51:
+         print("Wow! You must have quite the sweet tooth!")
+         print("Regardless of how sweet you like it, enjoy your cake!")
+      else: 
+         print("That is the perfect amount of sweetness!")
+         print("Regardless of how sweet you like it, enjoy your cake!")
     
 main()
