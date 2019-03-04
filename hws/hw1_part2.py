675534faceeb2e7ebbadfa2510f6ee4647acecaa
jae <jaekim2013@gmail.com>
2019-03-04 01:19:53 -0500

    Part 2 of hw1

---
+++ hws/hw1_part2.py
@@ -0,0 +1,10 @@
+# file: hw1_part2.py
+# author: Farming247
+# date: 3/4/2019
+
+def main():
+    numMoney = float(input("Enter your weekly allowance. "))
+    numNewphone = float(input("Enter the cost of a new phone. "))
+    numWeek = print("It will take you "+ str(numNewphone / numMoney) + " weeks")
+main()
+    
