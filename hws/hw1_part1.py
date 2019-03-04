ebcf3c7be228f1d9d5f2ce883e51ca1bff760a23
jae <jaekim2013@gmail.com>
2019-03-04 00:58:45 -0500

    Hw question 1

---
+++ hws/hw1_part1.py
@@ -0,0 +1,12 @@
+# File: hw_part1
+# Author: Farming247
+# Date: 2/14/2019
+
+def main():
+    numEmeralds = int(input("Total number of emeralds."))
+    numSaphires = int(input("Total number of saphires."))
+    numDiamonds = int(input("Total number of diamonds."))
+
+    gems = print("you have a total of " + str(numEmeralds + numSaphires + numDiamonds) + " gems in your chest.")
+main()
+
